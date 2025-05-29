095474b6c034c3dcbdb034afc344c7774153693c
Rituraj Kumar <rk4940@srmist.edu.in>
2025-05-29 19:06:03 +0530

    Update heap.cpp

--- heap.cpp
+++ heap.cpp
@@ -1,42 +1,38 @@
 #include<iostream>
-using namespace std;
+using  namespace std;
 class heap{
-        public:
-        int arr[100];
-        int size;
-        heap()
-        {
-            arr[0] = -1; // 0th index is not found
-             size = 0;
+    public:
+    int arr[100];
+    int size;
+    heap()
+    {
+        arr[0] = -1;
+        size = 0;
+    }
+    void insert(int val){
+        size = size + 1;
+        int index = size;
+        arr[index] = val;
+        while(index < 1){
+            int parent = index/2;
+            if(arr[parent] < arr[index]){
+                swap(arr[parent] , arr[index]);
+                index = parent;
+            }
         }
-        void insert(int val){
-             size = size + 1;
-             int index = size;
-             arr[index] = val;
-             // node end main daal di 
-             while(index > 1){
-                int parent = index/2;
-                if(arr[parent]<arr[index]){
-                    swap(arr[parent], arr[index]);
-                    index = parent;
-                }
-                else{
-                    return;
-                }
-             }
-        }
-        void print(){
-            for(int i = 1 ; i<=size; i++){
-                cout<<arr[i]<<" ";
-            } cout<<endl;
-        }
+    
+    }
+    void print(){
+        for (int i  = 1; i<= size; i++){
+            cout<< arr[i] << " ";
+        }cout << endl;
+    }
 };
 int main(){
     heap h;
-    h.insert(10);
-    h.insert(20);
-    h.insert(30);
-    h.insert(40);
     h.insert(50);
+    h.insert(55);
+    h.insert(38);
+    h.insert(68);
     h.print();
 }
