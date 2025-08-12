d90020bf2851ff8f1a980416ae89a4dab4601600
Rituraj Kumar <rk4940@srmist.edu.in>
2025-08-12 23:38:02 +0530

    Create bubblesort.cpp

---
+++ bubblesort.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+void bubblesort(int arr[] , int n){
+    for(int i =0; i<n-1; i++){
+        bool swapped = false; // Flag to check if any swapping occurred
+        for(int j =0; j<n-i-1; j++){
+            if(arr[j] > arr[j+1]){
+                swap(arr[j] , arr[j+1]);
+                swapped = true;
+            }
+        }
+        if(!swapped)
+        break;
+    }
+}
+int main(){
+    int n;
+    int arr[100];
+    cout << "Enter the number of elements: ";
+    cin>>n;
+    cout << "Enter the elements: ";
+    for(int i =0; i<n; i++){
+        cin>>arr[i];
+    }
+    bubblesort(arr,n);
+    cout << " bubble sorted Array : ";
+    for(int i =0; i<n ; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
