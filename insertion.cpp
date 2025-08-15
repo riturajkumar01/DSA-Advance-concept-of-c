17855e0c88b91983f34566eeefcd3176a2f73592
Rituraj Kumar <rk4940@srmist.edu.in>
2025-08-15 11:53:05 +0530

    Update insertion.cpp

--- insertion.cpp
+++ insertion.cpp
@@ -1,61 +1,31 @@
 #include<iostream>
 using namespace std;
-struct Node{
-    int key;
-    Node*left;
-    Node*right;
-    Node(int item){
-        //initializes the node with the given value and 
-        // sets its children as NULL (no children yet).
-        key = item;
-        left = NULL;
-        right = NULL;
+void insertionsort(int arr[] , int n){
+    for(int i=1; i<n; i++){
+        int temp = arr[i];
+        int j = i-1;
+        for(; j>=0; j--){
+            if(arr[j]> temp){
+                arr[j+1] = arr[j];
+            }
+        }
+        arr[j+1] = temp;
     }
-};
-Node* insert(Node* node , int key ){
-    // tree empty hain it means we can isnert the new  node 
-    if(node == NULL)
-        return new Node(key);
-        // key hain vo laready exist hain it means print vahi node 
-
-        if(node -> key == key )
-            return node;
-
-        if( node->key < key)
-        node->right = insert(node->right , key);
-    else 
-    node->left = insert(node->left , key );
-return node;  
-
 }
-// left , node , right // inorder 
-
-void inorder(Node* root){
-    if(root!=NULL){
-        inorder(root->left);
-        cout << root->key << " ";
-        inorder(root->right);
+int main(){
+    int n;
+    int arr[100];
+    cout << " Enter the number of elements : ";
+    cin>>n;
+    cout<<"enter the elements: ";
+    for(int i = 0; i<n; i++){
+        cin>>arr[i];
     }
- } 
-    
-int main() {
-    // Creating the following BST
-    //      50
-    //     /  \
-    //    30   70
-    //   / \   / \
-    //  20 40 60 80
-
-    Node* root = new Node(50);
-    root = insert(root, 30);
-    root = insert(root, 20);
-    root = insert(root, 40);
-    root = insert(root, 70);
-    root = insert(root, 60);
-    root = insert(root, 80);
-
-    // Print inorder traversal of the BST
-    inorder(root);
-
+   insertionsort(arr,n);
+    cout << "Bubble sorted Array: ";
+    for(int i =0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
     return 0;
 }
