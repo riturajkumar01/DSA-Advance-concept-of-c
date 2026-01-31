12c72df7f513466cb1f0109f605f5da548421588
Rituraj Kumar <rk4940@srmist.edu.in>
2026-01-31 13:38:03 +0530

    Create kahn's.cpp

---
+++ kahn's.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector
+#include<list>
+#include<queue>
+using namespace std;
+class Graph{
+    int V;
+    list<int>*l;
+    public:
+    Graph(int v){
+        this->V = v;
+        l = new list<int>[V];
+    }
+    void addedge(int u , int v){
+        l[u].push_back(v);
+    }
+    void topologicalsort(){
+        vector<int> res;
+        //calculate indegree
+        vector<int> indeg(V , 0);
+        for(int u = 0; u<V; u++){
+            for(int v : l[u]){
+                indeg[v]++;
+            }
+        }
+        queue<int> q;
+        for(int i =0 ; i<V; i++){
+            if(indeg[i] == 0){
+                q.push(i);
+            }
+        }
+        //bfs
+        while(q.size() > 0){
+            int u = q.front();  
+            q.pop();
+            res.push_back(u); // add the node to the topological order
+            for(int v : l[u]){
+                indeg[v]--;
+                if(indeg[v] == 0){
+                    q.push(v);
+                }
+            }
+
+    }
+}
+};
