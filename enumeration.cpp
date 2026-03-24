0c883649504ab3857546a1db6d3247fce80aadaf
Rituraj Kumar <rk4940@srmist.edu.in>
2026-03-24 16:20:28 +0530

    Create enumeration.cpp

---
+++ enumeration.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+enum WeekDays {MON = 1 , TUES , WED , THUR , FRI};
+int main(){
+    cout<< MON << endl;
+    cout<< TUES << endl;
+    cout<< FRI << endl;
+    return 0;
+}
